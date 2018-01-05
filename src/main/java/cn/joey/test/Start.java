f64abbd06d7711d8e520e362dcc19bc6dcf1b7b2
dunhanson <dunhanson@gmail.com>
2018-01-05 20:57:35 +0800

    完善Start例子

--- src/main/java/cn/joey/test/Start.java
+++ src/main/java/cn/joey/test/Start.java
@@ -9,28 +9,26 @@ import cn.joey.solr.entity.Sort;
 
 public class Start {
 	public static void main(String[] args) {
-		//q
+		//查询条件
 		List<Condition> q = new ArrayList<>();
 		q.add(new Condition("company_name", new String[] {"厦门"}));
-		//fq
+		//过滤条件
 		List<Condition> fq = new ArrayList<>();
 		fq.add(new Condition("province", new String[] {"福建"}));
-		//sort
+		//排序条件
 		List<Sort> sort = new ArrayList<>();
 		sort.add(new Sort("id"));
 		//分页对象
 		Pagination pagination = new Pagination(1, 30);
 		//查询对象
-		Query<Contact> query = new Query<Contact>(q, fq, sort, pagination, Contact.class);
+		Query<Contact> query = new Query<Contact>(q, fq, sort, pagination, Contact.class);	
+		//打印参数
 		System.out.println("q : " + query.getQStr());
 		System.out.println("fq : " + query.getFQStr());
-		System.out.println("sort : " + query.getSortStr());			
+		System.out.println("sort : " + query.getSortStr());				
 		//查询
 		query.search();
-		List<Contact> list = query.getResult();
-		for (Contact contact : list) {
-			System.out.println(contact);
-		}
-		System.out.println("size：" + list.size());
+		//分页信息
+		System.out.println(pagination);
 	}
 }
